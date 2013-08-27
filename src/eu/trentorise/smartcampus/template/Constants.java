433d388f8eef1b5be4ba90867267729e354927bb
raman <raman@fbk.eu>
2013-08-27 10:11:01 +0200

    Cleanup

--- src/eu/trentorise/smartcampus/template/Constants.java
+++ src/eu/trentorise/smartcampus/template/Constants.java
@@ -3,10 +3,10 @@ package eu.trentorise.smartcampus.template;
 public class Constants {
 
 	// PUT VALUES FOR YOU APPLICATION
-	public static final String CLIENT_ID = "";
-	public static final String CLIENT_SECRET = "";
+	public static final String CLIENT_ID = "e2b30183-f082-44ab-859f-4ea0ba9b758b";
+	public static final String CLIENT_SECRET = "562c7742-1ab8-4efa-981d-9a5d122c2679";
 
-	public static final String APPID = "";
+	public static final String APPID = "rk";
 
 	// SERVICES URLS
 
